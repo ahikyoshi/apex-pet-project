824432a4f97f4bac286f6ee77ce4843b95f96614
ahikyoshi <115409269+ahikyoshi@users.noreply.github.com>
2023-07-22 22:02:43 +0300

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css"
+
+function App() {
+
+  return (
+    <main className="bg-slate-100 flex flex-col items-center font-thin">
+      Hello app
+    </main>
+  )
+}
+
+export default App
